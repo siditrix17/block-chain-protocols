998b75aca9cd1b3af34234bd0ba3695f37bb5694
root <root@localhost.localdomain>
2017-07-19 22:48:57 -0400

    permission check(protocol-2)

---
+++ permission.py
@@ -0,0 +1,44 @@
+#permission check against block-chain
+#checkpolicy(PK-ksig,Xp)
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from ecdsa import SigningKey
+import base64
+import hashlib
+from Crypto import Random()
+from Crypto.Cipher import AES
+
+def encode(s):
+  # s is the message to be parsed
+ print"ENCODED string:",s.encode(encoding='base64',errors='strict')
+ return s
+ 
+def signpk():
+ pk = SigningKey.generate(curve=ecdsa.SECP256k1)
+ return pk
+ #this is PK(ksig)
+
+def hashing(pk):
+  m = hashlib.sha256()
+  m.update(pk)
+  A_policy=m.digest()
+  return A_policy
+
+def CheckPolicy():
+ allow=0
+ #to input list of permissions from user side 
+ POLICYus=['location','contacts','phone_number']    #policy contains permission required 
+ L=[]    #act as memory buffer
+ Xp=[]    #policies allowed by user
+ sign=signpk()
+ x=hashing(sign)
+ L.append(x)
+ if (L):
+   parse=encode(x)
+  if sign==pk or (sign==pk and Xp in POLICYus):
+    allow=1
+  else:
+    exit(0)
+ exit(0)
+
+CheckPolicy()
