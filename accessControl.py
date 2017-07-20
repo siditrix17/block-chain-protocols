56defca8c119398d8c678e1dbba122789f76dc09
Sourav Kar <souravkar3809@gmail.com>
2017-07-20 15:36:09 +0530

    Add files via upload
    Protocol-3: Access Control(Blockchain)

---
+++ accessControl.py
@@ -0,0 +1,36 @@
+#accessControlProtocol
+from ecdsa import SigningKey
+import base64
+import hashlib
+#from Crypto import Random()
+#from Crypto.Cipher import AES
+
+def encode(s):
+  # s is the message to be parsed
+ #print"ENCODED string:",s.encode(encoding='base64',errors='strict')
+ return s
+ 
+def signpk():
+ pk = SigningKey.generate(curve=ecdsa.SECP256k1)
+ return pk
+ #this is PK(ksig)
+
+def hashing(pk):
+  m = hashlib.sha256()
+  m.update(pk)
+  return m.digest()
+
+pk, pk2, sk = signkey()
+POLICY_us = CheckPolicy()
+L = []
+pk_k = signpk()
+hashed = hashing(pk_k)
+L.append(hashed)
+
+def handleAccessTX(pk_k, message):
+	allow = 0
+	parsed = encode(message)
+	if pk_k == pk_us:
+		L[hashed] = message
+		allow = 1
+	return allow
